e0b9d4e2038a01a919fd7677a02010e82640408d
Diego <diego_11404@hotmail.com>
2017-11-23 20:09:45 -0500

    update contants

---
+++ Constants.hpp
@@ -0,0 +1,59 @@
+//
+//  Constants.hpp
+//  PROYECTO
+//
+//  Created by DevJAD on 1/11/17.
+//  Copyright © 2017 DevJAD. All rights reserved.
+//
+#pragma once
+
+#define SCREEN_WIDTH 1324
+#define SCREEN_HEIGHT 1024
+
+#define MEDIUM_SCREEN_SIZE_WIDTH 800
+#define LARGE_SCREEN_SIZE_WIDTH  1324
+#define MEDIUM_SCREEN_SIZE_HEIGHT 600
+#define LARGE_SCREEN_SIZE_HEIGHT  1024
+
+#define FLASH_SPEED 300.0f
+#define GAME_OVER_AFTER_FLASH 900.f
+
+#define GAME_CHARACTER01_PATH "Assets/images/character01.png"
+#define GAME_CHARACTER02_PATH "Assets/images/character02.png"
+#define GAME_CHARACTER03_PATH "Assets/images/character03.png"
+
+#define GAME_MOVE_TO_UP 0
+#define GAME_MOVE_TO_DOWN 1
+#define GAME_MOVE_TO_LEFT 2
+#define GAME_MOVE_TO_RIGTH 3
+#define IS_SWIMMING 4
+#define IS_ATTACKING 5
+#define IS_DEAD 6
+#define IS_GAME_OVER 7
+#define IS_PLAYING 8
+// ESCENARIOS
+
+// splash
+#define SPASH_BACKGROUND_FILEPATH ""
+#define SPLASH_EXTRA_FILEPATH "Assets/images/extra_splash.png"
+#define SPLASH_LOGO_FILEPATH "Assets/images/logo_splash.png"
+#define SPLASH_DURATION 4.0
+
+// main
+#define MAIN_BACKGROUND_FILEPATH "Assets/images/main_background.jpg"
+#define MAIN_MENU_FILEPATH "Assets/images/main_menu_sprite.png"
+#define MAIN_TITLE_FILEPATH "Assets/images/main_title.png"
+
+// sea
+#define SEA_BACKGROUND_FILEPATH "Assets/images/sea_background.png"
+#define SEA_CORAL_FILEPATH "Assets/images/sea_coral.png"
+
+//game over
+#define GAME_OVER_BOX_FILEPATH "Assets/images/game_over.png"
+#define GAME_OVER_BUTTONS_FILEPATH "Assets/images/game_over_buttons.png"
+// ENTIDADES
+#define SHARK_FILEPATH "Assets/images/shark.png"
+#define SHARK_SWIMMING 0
+#define SHARK_DIE 1
+#define SHARK_ATTACK 2
+
